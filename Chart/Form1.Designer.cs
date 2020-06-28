348346da440e8a818a7999a2db6e83f7ebc98bb8
ermias <ermiaswoldetsadik@gmail.com>
2020-06-27 23:27:30 -0400

    added buttons

--- Chart/Form1.Designer.cs
+++ Chart/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Chart
 {
-    partial class Form1
+    partial class Chart
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,190 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.SalesByYearChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.FromComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.GoButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ToComboBox = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.SalesByYearChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SalesByYearChart
+            // 
+            this.SalesByYearChart.BorderlineColor = System.Drawing.Color.Black;
+            this.SalesByYearChart.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
+            this.SalesByYearChart.BorderlineWidth = 3;
+            chartArea1.Name = "ChartArea1";
+            this.SalesByYearChart.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.SalesByYearChart.Legends.Add(legend1);
+            this.SalesByYearChart.Location = new System.Drawing.Point(12, 77);
+            this.SalesByYearChart.Name = "SalesByYearChart";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.SalesByYearChart.Series.Add(series1);
+            this.SalesByYearChart.Size = new System.Drawing.Size(776, 352);
+            this.SalesByYearChart.TabIndex = 0;
+            this.SalesByYearChart.Text = "chart1";
+            title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            title1.ForeColor = System.Drawing.Color.Red;
+            title1.Name = "Sales By Year";
+            title1.Text = "Sales By Year";
+            this.SalesByYearChart.Titles.Add(title1);
+            // 
+            // FromComboBox
+            // 
+            this.FromComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.FromComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FromComboBox.FormattingEnabled = true;
+            this.FromComboBox.Items.AddRange(new object[] {
+            "1990",
+            "1991",
+            "1992",
+            "1993",
+            "1994",
+            "1995",
+            "1996",
+            "1997",
+            "1998",
+            "1999",
+            "2000",
+            "2001",
+            "2002",
+            "2003",
+            "2004",
+            "2005",
+            "2006",
+            "2007",
+            "2008",
+            "2009",
+            "2010",
+            "2011",
+            "2012",
+            "2013",
+            "2014",
+            "2015",
+            "2016",
+            "2017",
+            "2018",
+            "2019"});
+            this.FromComboBox.Location = new System.Drawing.Point(81, 29);
+            this.FromComboBox.Name = "FromComboBox";
+            this.FromComboBox.Size = new System.Drawing.Size(227, 33);
+            this.FromComboBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "From:";
+            // 
+            // GoButton
+            // 
+            this.GoButton.Location = new System.Drawing.Point(667, 29);
+            this.GoButton.Name = "GoButton";
+            this.GoButton.Size = new System.Drawing.Size(121, 33);
+            this.GoButton.TabIndex = 3;
+            this.GoButton.Text = "Go";
+            this.GoButton.UseVisualStyleBackColor = true;
+            this.GoButton.Click += new System.EventHandler(this.GoButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(338, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "To:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // ToComboBox
+            // 
+            this.ToComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ToComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ToComboBox.FormattingEnabled = true;
+            this.ToComboBox.Items.AddRange(new object[] {
+            "1990",
+            "1991",
+            "1992",
+            "1993",
+            "1994",
+            "1995",
+            "1996",
+            "1997",
+            "1998",
+            "1999",
+            "2000",
+            "2001",
+            "2002",
+            "2003",
+            "2004",
+            "2005",
+            "2006",
+            "2007",
+            "2008",
+            "2009",
+            "2010",
+            "2011",
+            "2012",
+            "2013",
+            "2014",
+            "2015",
+            "2016",
+            "2017",
+            "2018",
+            "2019"});
+            this.ToComboBox.Location = new System.Drawing.Point(386, 29);
+            this.ToComboBox.Name = "ToComboBox";
+            this.ToComboBox.Size = new System.Drawing.Size(227, 33);
+            this.ToComboBox.TabIndex = 4;
+            // 
+            // Chart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ToComboBox);
+            this.Controls.Add(this.GoButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FromComboBox);
+            this.Controls.Add(this.SalesByYearChart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Chart";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chart";
+            this.Load += new System.EventHandler(this.Chart_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.SalesByYearChart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart SalesByYearChart;
+        private System.Windows.Forms.ComboBox FromComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button GoButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox ToComboBox;
     }
 }
 
--- Chart/Form1.cs
+++ Chart/Form1.cs
@@ -1,20 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace Chart
 {
-    public partial class Form1 : Form
+    public partial class Chart : Form
     {
-        public Form1()
+        public Chart()
         {
             InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Chart_Load(object sender, EventArgs e)
+        {
+            FromComboBox.Text = "2012";
+            ToComboBox.Text = "2015";
+
+            SalesByYearChart.DataSource = GetData();
+            SalesByYearChart.Series["Series1"].Points.Clear();
+            SalesByYearChart.Series["Series1"].XValueMember = "Year";
+            SalesByYearChart.Series["Series1"].YValueMembers = "Total";
+        }
+
+
+
+        // dataset,object,array,collection,generic,datatable
+        private DataTable GetData()
+        {
+            DataTable dtChartData = new DataTable();
+
+            using(SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["dbx"].ConnectionString))
+            {
+                using(SqlCommand cmd = new SqlCommand("Orders1", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    conn.Open();
+
+                    cmd.Parameters.AddWithValue("@FromYear", FromComboBox.Text);
+                    cmd.Parameters.AddWithValue("@ToYear", ToComboBox.Text);
+
+                   //SqlDataReader reader = cmd.ExecuteReader();
+
+                    //dtChartData.Load(reader);
+                }
+            }
+            return dtChartData;
+        }
+
+        private void GoButton_Click(object sender, EventArgs e)
+        {
+            SalesByYearChart.DataSource = GetData();
+            SalesByYearChart.Series["Series1"].Points.Clear();
+            SalesByYearChart.Series["Series1"].XValueMember = "Year";
+            SalesByYearChart.Series["Series1"].YValueMembers = "Total";
         }
     }
 }
